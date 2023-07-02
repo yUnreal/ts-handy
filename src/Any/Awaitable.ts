08c25eaeee4ad3d3c5effaf59afca624bcc181bc
yUnreal <unrealwebdev@gmail.com>
2023-07-02 19:15:12 -0300

    fix(type): Fixing Awaitable type and adding comment to it

--- src/Any/Awaitable.ts
+++ src/Any/Awaitable.ts
@@ -1,5 +1,15 @@
 /**
+ * An value that can be awaitable or not
+ * @param {unknown} P - The value
+ * @memberof Any
+ * @example
+ * import { Any } from 'ts-toolset';
  * 
+ * async function bar(foo: Awaitable<string>): Promise<void> {
+ *   const result: Any.Await<typeof foo> = await foo;
+ * 
+ *   console.log(result);
+ * }
  */
 
-export type Awaitable<P> = Promise<P> | P;
+export type Awaitable<P> = PromiseLike<P> | P;
