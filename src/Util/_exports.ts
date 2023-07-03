de6d5c4c17e0236cbb0c3f897f670dbf9f30a6bd
yUnreal <unrealwebdev@gmail.com>
2023-07-03 15:16:55 -0300

    feat(type): Adding new/old type to Util types

--- src/Util/_exports.ts
+++ src/Util/_exports.ts
@@ -3,3 +3,4 @@ export { Writable } from './Writable';
 export { IsEmpty } from './isEmpty';
 export { DeepWritable } from './DeepWritable';
 export { DeepReadonly } from './DeepReadonly';
+export { Length } from './Length';
