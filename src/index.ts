9984304d1a29eeab82c666cc0f9c6aadaf2d8b46
yUnreal <unrealwebdev@gmail.com>
2023-07-04 18:26:06 -0300

    feat(export): Forgot to export new function types

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export * as Array from './Array/_exports';
 export * as Class from './Class/_exports';
 export * as String from './String/_exports';
 export * as Boolean from './Boolean/_exports';
+export * as Function from './Function/_exports';
