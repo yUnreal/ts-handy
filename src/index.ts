c23ed344d3e4073ba4bf5e56cdfe675f50a0576d
yUnreal <unrealwebdev@gmail.com>
2023-07-03 21:03:41 -0300

    feat(export): Exporting all new types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * as Any from './Any/_exports';
 export * as Util from './Util/_exports';
 export * as Array from './Array/_exports';
+export * as Class from './Class/_exports';
 export * as String from './String/_exports';
