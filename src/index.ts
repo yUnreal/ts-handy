cc5dc326f38d778c63f4863d6b8415f322bcea46
yUnreal <unrealwebdev@gmail.com>
2023-07-06 17:19:42 -0300

    fix(export): Forgot to remove Util exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export * as Any from './Any/_exports';
-export * as Util from './Util/_exports';
 export * as Array from './Array/_exports';
 export * as Class from './Class/_exports';
 export * as String from './String/_exports';
