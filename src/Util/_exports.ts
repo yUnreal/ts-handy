7485284c14fe5de25e14e01f6eb68a8120cc5927
yUnreal <unrealwebdev@gmail.com>
2023-07-06 17:11:08 -0300

    fix(file): Using the correct case in file name

--- src/Util/_exports.ts
+++ src/Util/_exports.ts
@@ -1,6 +1,6 @@
 export { IsAny } from './IsAny';
 export { Length } from './Length';
-export { IsEmpty } from './isEmpty';
+export { IsEmpty } from './IsEmpty';
 export { Writable } from './Writable';
 export { IsNotAny } from './IsNotAny';
 export { EmptyObject } from './EmptyObject';
