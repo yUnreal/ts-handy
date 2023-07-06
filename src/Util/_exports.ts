cb5bd2600ae906e79ae5126a1608731e708f42e7
yUnreal <unrealwebdev@gmail.com>
2023-07-06 17:19:24 -0300

    BREAKING CHANGE: Now Util is Any when importing

--- src/Util/_exports.ts
+++
@@ -1,8 +0,0 @@
-export { IsAny } from './IsAny';
-export { Length } from './Length';
-export { IsEmpty } from './IsEmpty';
-export { Writable } from './Writable';
-export { IsNotAny } from './IsNotAny';
-export { EmptyObject } from './EmptyObject';
-export { DeepWritable } from './DeepWritable';
-export { DeepReadonly } from './DeepReadonly';
