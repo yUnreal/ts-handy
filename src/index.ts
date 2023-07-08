27902d4713bb45d4c16164bd0bcb9af2defdb97f
yUnreal <unrealwebdev@gmail.com>
2023-07-08 10:02:20 -0300

    chore(src/index.ts): Now it imports all

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * as Any from './Any/_exports';
 export * as Array from './Array/_exports';
 export * as Class from './Class/_exports';
+export { version } from '../package.json';
 export * as String from './String/_exports';
 export * as Boolean from './Boolean/_exports';
 export * as Function from './Function/_exports';
